ee0241d718b8f758d73c88ff7c3f743b96a31455
Hassnian <hassnianxd@gmail.com>
2019-06-02 12:26:59 +0200

    [ADD] store testing

--- src/tests/ad.test.js
+++ src/tests/ad.test.js
@@ -1,9 +1,49 @@
-import { exportAllDeclaration } from "@babel/types";
+const mongoose = require("mongoose");
+const URI = "mongodb://localhost/ads-test"; // put your url connection here
+mongoose.connect(URI, { useNewUrlParser: true });
+const { store, show } = require("../controllers/ad.controller");
+const Ad = require("../models/ad");
 
-import { exportAllDeclaration } from "@babel/types";
+//to make sure that there are not false positives
+test("ads test", () => {
+  beforeEach(async () => {
+    await Ad.deleteMany({});
+  });
 
-function hello(){
- expect   
-}
+  afterEach(async () => {
+    await Ad.deleteMany({});
+  });
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
 
-console.log("test")
+// test("gets all ads", async () => {
+//   const ads = await show();
+//   console.log(ads);
+//   expect(true).toBe(true);
+// });
+
+test("store an ad", async () => {
+  const newAd = new Ad({ title: "selling something", description: "123" });
+  await store(newAd);
+  const expected = 1;
+  const actual = await Ad.where({ _id: newAd["_id"] }).countDocuments();
+  expect(expected).toEqual(actual);
+});
+
+test("store fails when title is longer than 50 ", async () => {
+    const newAd = new Ad({ title: "012345678910123456789101234567891012345678910123456", description: "123" });
+    await store(newAd);
+    const expected = 0;
+    const actual = await Ad.where({ _id: newAd["_id"] }).countDocuments();
+    expect(expected).toEqual(actual);
+  });
+
+  test("store fails when title is longer than 50 ", async () => {
+    const newAd = new Ad({ title: "same", description: "same" });
+    await store(newAd);
+    const expected = 0;
+    const actual = await Ad.where({ _id: newAd["_id"] }).countDocuments();
+    expect(expected).toEqual(actual);
+  });
